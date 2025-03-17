e8c7e95216e5f377aca9ab90be80da9a5fc034bd
Mihir Dagar <194290818+DagarMihir@users.noreply.github.com>
2025-03-17 19:44:17 +0530

    Added some files 0 to 4 in basics.

---
+++ 01-basics/0-namaste-duniya.cpp
@@ -0,0 +1,14 @@
+// Program to print "Hello World!!" 
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Namaste Dunia!!" << endl;
+
+}
+
+
+// endl can be replaced with '\n', used to move cursor to next line.
---
+++ 01-basics/1-data-types.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+/*
+    int a = 123;
+    cout << a << endl;
+
+    char b = 'a'; //only single character i.e. 'ab' is invalid
+     cout << b << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    float f = 1.2;
+    cout << f << endl;
+
+    double d = 1.23;
+    cout << d << endl;
+
+    int size = sizeof(a);
+    cout << "Size of a (int) is : " << size << endl;
+
+
+
+    // Type Casting
+
+    int a = 'a';
+    cout << a << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+
+    //here 123456 is out of bound for char data type, thus it prints @ which is 64 in ascii, it stores the last bits in the memory.
+
+    // Signed(Default) and Unsigned int (Only stores +ve numbers).
+
+
+*/
+
+    unsigned int a = -112;
+
+    cout << a << endl;
+
+}
